d89b1f3400fda4d9e5879ac95e64d7f4758831fe
a.omari <a.omari@atlanticxchange.com>
2025-03-29 11:56:30 -0400

    increased context timeout for migrations

--- MudBlazorWebApp1/Program.cs
+++ MudBlazorWebApp1/Program.cs
@@ -36,7 +36,7 @@ builder.Services.Configure<CircuitOptions>(options => options.DetailedErrors = t
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(connectionString, sqlServerOptions => sqlServerOptions.CommandTimeout(120)));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentity<User, Role>(options =>
